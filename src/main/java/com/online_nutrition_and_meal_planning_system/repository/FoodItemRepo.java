05e4575c3a4e7d3baa7e5d7e8df569d7946c9aad
Ajai-L09 <ajai.l2024cse@sece.ac.in>
2025-10-29 15:48:40 +0530

    Add files via upload

--- src/main/java/com/online_nutrition_and_meal_planning_system/repository/FoodItemRepo.java
+++ src/main/java/com/online_nutrition_and_meal_planning_system/repository/FoodItemRepo.java
@@ -2,7 +2,9 @@ package com.online_nutrition_and_meal_planning_system.repository;
 
 import com.online_nutrition_and_meal_planning_system.model.FoodItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodItemRepo extends JpaRepository<FoodItem, Long> {
 
 }
--- src/main/java/com/online_nutrition_and_meal_planning_system/repository/UserRepo.java
+++ src/main/java/com/online_nutrition_and_meal_planning_system/repository/UserRepo.java
@@ -2,9 +2,10 @@ package com.online_nutrition_and_meal_planning_system.repository;
 
 import com.online_nutrition_and_meal_planning_system.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
